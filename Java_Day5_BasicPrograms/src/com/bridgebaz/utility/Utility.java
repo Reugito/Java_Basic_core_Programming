b7bb994c7ee749e8c525bb3ead4906817ab863f0
Reugito <raosahebdhotreedu@gmail.com>
2021-06-25 20:13:18 +0530

    added comments in utility class

--- Java_Day5_BasicPrograms/src/com/bridgebaz/utility/Utility.java
+++ Java_Day5_BasicPrograms/src/com/bridgebaz/utility/Utility.java
@@ -71,7 +71,7 @@ public class Utility {
 			} 
 		}
 	}
-
+	// method to get harmonic number
 	public void getHarmonicNumber(int N) {
 		double Harmonicnumber = 0;
 		
@@ -80,14 +80,14 @@ public class Utility {
 		}
 		System.out.println("Nth Harmonic Number of "+N+" = "+Harmonicnumber);
 	}
-	
+	// method to get largest among three number
 	public void getLargestNumber(int num1, int num2, int num3) {
 		int FirstCond, SecondCond;
 		FirstCond = (num1>num2)? num1:num2; 
 		SecondCond = (FirstCond>num3)? FirstCond:num3; 
 		System.out.println(SecondCond+" is greatest among "+num1+", "+num2+", "+num3);
 	}
-	
+	// method to get quetient and remainder 
 	public void getQuotientRemainder(int Divisor, int Dividend ) {
 		int Quotient, Remainder;
 		Remainder = Dividend % Divisor;
